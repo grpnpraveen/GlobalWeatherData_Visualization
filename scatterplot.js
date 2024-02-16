348cd701fd92e9e1d5f719a698a619ae873d4bf3
Gali Ravi Praveen <grpnpraveen@gmail.com>
2024-02-15 21:12:00 -0500

    jitter

--- scatterplot.js
+++ scatterplot.js
@@ -3,9 +3,9 @@ function createScatterPlot(xAttribute, yAttribute) {
     document.getElementById("visualization").innerHTML = "";
 
     // Set up dimensions and margins
-    const margin = { top: 50, right: 30, bottom: 100, left: 60 }; // Increased bottom margin for labels
-    const width = 1000 - margin.left - margin.right;
-    const height = 600 - margin.top - margin.bottom;
+    const margin = { top: 50, right: 30, bottom: 200, left: 200 }; // Increased bottom margin for labels
+    const width = 1020 - margin.left - margin.right;
+    const height = 800 - margin.top - margin.bottom;
 
     const svg = d3.select("#visualization")
         .append("svg")
@@ -22,7 +22,7 @@ function createScatterPlot(xAttribute, yAttribute) {
     }
 
     let xScale, yScale;
-    let cat_data1,cat_data2;
+    // let cat_data1,cat_data2;
     let temp_data={};
     // Create scales with padding
     xScale = d3.scaleLinear()
@@ -42,42 +42,38 @@ function createScatterPlot(xAttribute, yAttribute) {
         if(data_info[xAttribute].type=="categorical")
         {
             console.log("X cat");
-            cat_data1=Object.fromEntries(get_unique(data,xAttribute).map((str, index) => [str, index]))
-            console.log(cat_data1);
-            // cat_data1 = get_graph_data(xAttribute);
-            // Create scales
-            xScale = d3.scaleLinear()
-            .domain([0, Object.entries(cat_data1).length])
-            .range([0, width]);
+        const uniqueCategoriesX = get_unique(data, xAttribute);
+        xScale = d3.scaleBand()
+            .domain(uniqueCategoriesX)
+            .range([0, width])
+            .padding(0.1); // Adjust padding between bars as needed
         }
         if(data_info[yAttribute].type=="categorical")
         {
-            console.log("Y cat");
-            cat_data2=Object.fromEntries(get_unique(data,yAttribute).map((str, index) => [str, index]))
-            console.log(cat_data2);
-
-            yScale = d3.scaleLinear()
-            .domain([0,Object.entries(cat_data2).length]) // Adjusted to use the min and max of the data
-            .range([height, 0])
-            .nice(); // Ensures a "nice" scale with round numbers
+            console.log("X cat");
+        const uniqueCategoriesY = get_unique(data, yAttribute);
+        yScale = d3.scaleBand()
+            .domain(uniqueCategoriesY)
+            .range([height,0])
+            .padding(0.1); // Adjust padding between bars as needed
         }
 
         // PLOTTING 
-        if(cat_data1 && !cat_data2)  // first cat second num
+        if(data_info[xAttribute].type=="categorical" && data_info[yAttribute].type!="categorical")  // first cat second num
         {
             console.log("1cat 2 num");
             // Draw dots
             svg.selectAll(".dot")
-            .data(data)
-            .enter().append("circle")
-            .attr("class", "dot")
-            .attr("cx", d => xScale(cat_data1[d[xAttribute]]))
-            .attr("cy", d => yScale(parseInt(d[yAttribute])))
-            .attr("r", 4)
-            .attr("fill", "steelblue")
-            .attr("opacity", 0.8);
+                .data(data)
+                .enter().append("circle")
+                .attr("class", "dot")
+                .attr("cx", d => xScale(d[xAttribute]) + xScale.bandwidth() / 2) // Centering the dots within each category
+                .attr("cy", d => yScale(parseInt(d[yAttribute])))
+                .attr("r", 4)
+                .attr("fill", "steelblue")
+                .attr("opacity", 0.8);
         }
-        if(!cat_data1 && cat_data2)  // first num second cat
+        if(data_info[xAttribute].type!="categorical" && data_info[yAttribute].type=="categorical")  // first num second cat
         {
             console.log("1 num 2 cat");
             // Draw dots
@@ -86,13 +82,13 @@ function createScatterPlot(xAttribute, yAttribute) {
             .enter().append("circle")
             .attr("class", "dot")
             .attr("cx", d => xScale(parseInt(d[xAttribute])))
-            .attr("cy", d => yScale(cat_data2[d[yAttribute]]))
+            .attr("cy", d => yScale(d[yAttribute]) + yScale.bandwidth()/2 )
             .attr("r", 4)
             .attr("fill", "steelblue")
             .attr("opacity", 0.8);
          
         }
-        if(!cat_data1 && !cat_data2) // two num
+        if(data_info[xAttribute].type!="categorical" && data_info[yAttribute].type!="categorical") // two num
         {
             console.log("2 num");
             // Draw dots
@@ -106,55 +102,57 @@ function createScatterPlot(xAttribute, yAttribute) {
                 .attr("fill", "steelblue")
                 .attr("opacity", 0.8);
         }
-        if(cat_data1 && cat_data2) // two cat
+        if(data_info[xAttribute].type=="categorical" && data_info[yAttribute].type=="categorical") // two cat
         {
             console.log("2 cat");
             // Draw dots
-            // svg.selectAll(".dot")
-            // .data(data)
-            // .enter().append("circle")
-            // .attr("class", "dot")
-            // .attr("cx", d => xScale(cat_data1[d[xAttribute]]))
-            // .attr("cy", d => yScale(cat_data2[d[yAttribute]]))
-            // .attr("r", 4)
-            // .attr("fill", "steelblue")
-            // .attr("opacity", 0.8);
+           // Define a random number generator following a normal distribution
+const normalRandom = d3.randomNormal(0, 5); // Adjust the standard deviation as needed
 
+svg.selectAll(".cluster")
+    .data(data)
+    .enter().append("g")
+    .attr("class", "cluster")
+    .attr("transform", d => `translate(${xScale(d[xAttribute])}, ${yScale(d[yAttribute])})`)
+    .selectAll(".dot")
+    .data(d => Array.from({ length: 25 })) // Adjust the number of points in each cluster
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("cx", () => normalRandom())
+    .attr("cy", () => normalRandom())
+    .attr("r", 2)
+    .attr("fill", "steelblue")
+    .attr("opacity", 0.8);
 
-            svg.selectAll(".cluster")
-            .data(data)
-            .enter().append("g")
-            .attr("class", "cluster")
-            .attr("transform", d => `translate(${xScale(cat_data1[d[xAttribute]])}, ${yScale(cat_data2[d[yAttribute]])})`)
-            .selectAll(".dot")
-            .data(d => Array.from({ length: 25 })) // Adjust the number of points in each cluster
-            .enter().append("circle")
-            .attr("class", "dot")
-            .attr("cx", () => Math.random() * 30) // Random displacement for x-coordinate within the cluster
-            .attr("cy", () => Math.random() * 30) // Random displacement for y-coordinate within the cluster
-            .attr("r", 2)
-            .attr("fill", "steelblue")
-            .attr("opacity", 0.8);
 
         }
 
 
-    // Draw x-axis
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(xScale));
+// Draw x-axis
+svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale))
+    .selectAll("text")
+    .style("font-size", "9px") // Change font size here
+    .attr("transform", "rotate(-45)")
+    .attr("dy", "-0.5em")
+    .attr("dx", "-1em")
+    .style("text-anchor", "end");
+
 
     // Add x-axis label
     svg.append("text")
         .attr("x", width / 2)
-        .attr("y", height + margin.top + 20) // Adjusted y position for label
+        .attr("y", height + margin.top + 70) // Adjusted y position for label
         .style("text-anchor", "middle")
         .text(data_info[xAttribute]["x_title"])
         .attr("fill", "white");
 
     // Draw y-axis
     svg.append("g")
-        .call(d3.axisLeft(yScale));
+        .call(d3.axisLeft(yScale))
+        .selectAll("text")
+        .style("font-size", "10px") // Change font size here;
 
     // Add y-axis label
     svg.append("text")
